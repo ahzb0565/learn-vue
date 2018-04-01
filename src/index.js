d8ed8173e98e566f38b62e9bd9ad58c6d6ec1125
Bob <ahzb0565@sina.com>
2018-04-02 00:50:19 +0800

    conditionals and loops

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import Vue from 'vue';
 import NavBar from './components/nav';
 
+const HEROES = [
+    {name: 'Bob', age: 15, title:'dev'},
+    {name: 'Jim', age: 15, title:'Tester'},
+    {name: 'Benjamin', age: 15, title:'PM'},
+    {name: 'Justin', age: 15, title:'Ops'}
+]
+
 new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!',
-        title: 'You visit this one on ' + new Date().toLocaleString()
+        title: 'You visit this one on ' + new Date().toLocaleString(),
+        heroes: HEROES
     }
 });
 
