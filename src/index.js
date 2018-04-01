e0f5ae758688854aa303f6e120fa95bdeab40232
Bob <ahzb0565@sina.com>
2018-04-02 01:09:58 +0800

    Component and data passing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import NavBar from './components/nav';
 
+Vue.component('hero-details', {
+    props: ['hero'],
+    template: '<li>Name: {{ hero.name }}, age: {{ hero.age }}</li>'
+})
 
 new Vue({
     el: '#app',
@@ -8,10 +12,10 @@ new Vue({
         message: 'Hello Vue!',
         title: 'You visit this one on ' + new Date().toLocaleString(),
         heroes: [
-            {name: 'Bob', age: 15, title:'dev'},
-            {name: 'Jim', age: 15, title:'Tester'},
-            {name: 'Benjamin', age: 15, title:'PM'},
-            {name: 'Justin', age: 15, title:'Ops'}
+            {id: 0, name: 'Bob', age: 15, title:'dev'},
+            {id: 1, name: 'Jim', age: 15, title:'Tester'},
+            {id: 2, name: 'Benjamin', age: 15, title:'PM'},
+            {id: 3, name: 'Justin', age: 15, title:'Ops'}
         ],
         comments: ""
     }
@@ -21,4 +25,5 @@ new Vue({
     el: '#navbar',
     components: { NavBar },
     template: '<NavBar />' // This can't be <Nav />, because <nav> is known tag in html
-})
+});
+
