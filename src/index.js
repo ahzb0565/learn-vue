a522149c39e083adaff331d6b8926e575f548f13
Bob <ahzb0565@sina.com>
2018-04-03 00:26:21 +0800

    event handling

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import NavBar from './components/nav';
 
 Vue.component('hero-details', {
     props: ['hero'],
-    template: '<li>Name: {{ hero.name }}, age: {{ hero.age }}</li>'
+    template: '<li>Name: {{ hero.name }}, age: {{ hero.age }} <button v-on:click="remove">remove</button></li>',
+    methods: {
+        remove: function(even){
+            this.$emit('remove');
+        }
+    }
 })
 
-new Vue({
+var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!',
@@ -17,9 +22,45 @@ new Vue({
             {id: 2, name: 'Benjamin', age: 15, title:'PM'},
             {id: 3, name: 'Justin', age: 15, title:'Ops'}
         ],
-        comments: ""
+        newPerson: {
+            name: '',
+            age: 0
+        },
+        comments: "",
+    },
+    created: function(){
+        console.log(`The comments is "${this.comments}" by default`);
+    },
+    methods: {
+        addPerson: function(){
+            this.heroes.push({
+                id: this.heroes.length,
+                name: this.newPerson.name,
+                age: this.newPerson.age,
+                title: 'new one'
+            });
+            this.newPerson = {name: '', age: 0};
+        },
+        removePerson: function(id){
+            let hero = this.heroes.filter(person => person.id == id);
+            if (hero.length != 1){
+                console.error('Error, hero not found with id ' + id);
+                return
+            };
+            console.log('Remove hero: '+ hero[0].name);
+            this.heroes.splice(this.heroes.indexOf(hero[0]), 1);
+        }
     }
 });
+
+app.$watch('comments', function(oldVar, newVar){
+    console.log(`Comments updated ${oldVar} -> ${newVar}`);
+});
+app.$watch('heroes', function(oldVar, newVar){
+    let heroesPrev = oldVar.map(hero => hero.name);
+    let heroesNew = newVar.map(hero => hero.name);
+    console.log(`heroes updated ${heroesPrev} -> ${heroesNew}`);  // TODO: fix me, incorrect
+})
 
 new Vue({
     el: '#navbar',
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+let openInEditor = require('launch-editor-middleware')
 
 module.exports = {
+    mode: "development",
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
@@ -8,7 +10,10 @@ module.exports = {
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
-        port: 9000
+        port: 9000,
+        before(app){
+            app.use('/__open-in-editor', openInEditor())
+        }
     },
     module: {
         rules: [
